02afeb06157d2efcabfcdf931b077a23615e54fa
Masafumi Yokoyama <myokoym@gmail.com>
2013-06-09 12:40:59 +0900

    Create

---
+++ config.ru
@@ -0,0 +1,3 @@
+require File.expand_path(File.dirname(__FILE__)) + '/rab2pdf'
+
+run Sinatra::Application
---
+++ rab2pdf.rb
@@ -0,0 +1,35 @@
+require "sinatra"
+require "haml"
+require "fileutils"
+require "rabbit/command/rabbit"
+
+get "/" do
+  haml :index
+end
+
+post "/convert" do
+  begin
+    @download_url = convert(params)
+    @params = params
+    haml :index
+  rescue => e
+    "Error: #{e}"
+  end
+end
+
+private
+def convert(params)
+  base_dir = "public/pdf"
+  FileUtils.mkdir_p(base_dir)
+  pdf_path = File.join(base_dir, params[:filename])
+
+  Tempfile.open(["rab2pdf", ".rab"]) do |tempfile|
+    tempfile.puts(params[:source])
+    tempfile.flush
+    Rabbit::Command::Rabbit.run("--print",
+                                "--output-filename", pdf_path,
+                                tempfile.path)
+  end
+
+  File.join("http://myokoym.net/rab2pdf", pdf_path)
+end
