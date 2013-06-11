30a9a6354584ba3a03727c4777b3a1a60d94c97a
Masafumi Yokoyama <myokoym@gmail.com>
2013-06-11 22:49:31 +0900

    Support git repository

--- rab2pdf.rb
+++ rab2pdf.rb
@@ -1,6 +1,7 @@
 require "sinatra"
 require "haml"
 require "fileutils"
+require "tmpdir"
 require "rabbit/command/rabbit"
 
 get "/" do
@@ -12,6 +13,14 @@ post "/convert" do
     @download_url = convert(params)
     @params = params
     haml :index
+  rescue => e
+    "Error: #{e}"
+  end
+end
+
+get "/git" do
+  begin
+    redirect git(params[:url])
   rescue => e
     "Error: #{e}"
   end
@@ -39,4 +48,33 @@ def convert(params)
   end
 
   File.join("http://myokoym.net/rab2pdf", "pdf", today, filename)
+end
+
+def git(url)
+  download_url = nil
+
+  Dir.mktmpdir do |tmpdir|
+    FileUtils.cd(tmpdir) do
+      system("git", "clone", "--quiet", url)
+    end
+
+    repo_name = File.basename(url, ".git")
+    repo_path = File.join(tmpdir, repo_name)
+    rab_name = File.open(File.join(repo_path, ".rabbit")).read.chomp
+
+    pdf_name = rab_name.gsub(/\.\w+\z/, ".pdf")
+    download_url = File.join("http://myokoym.net/rab2pdf", "pdf", today, pdf_name)
+    today = Time.now.strftime("%Y%m%d")
+    base_dir = File.expand_path("public/pdf/#{today}")
+    FileUtils.mkdir_p(base_dir)
+    pdf_path = File.join(base_dir, pdf_name)
+
+    FileUtils.cd(repo_path) do
+      system("rabbit", "--print",
+                       "--output-filename", pdf_path,
+                       rab_name)
+    end
+  end
+
+  download_url
 end
